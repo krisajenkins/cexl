7546de449420eedc9acc4fabe02996a82dd04b4c
Kris Jenkins <krisajenkins@gmail.com>
2013-12-01 12:26:01 +0000

    Stub setup for the evaluator.

---
+++ src/evaluator.js
@@ -0,0 +1,10 @@
+/*global exports: true*/
+"use strict";
+
+var evaluate = function (expr) {
+    throw new Error("Cannot evaluate expression: " + expr);
+};
+
+exports.evaluate = evaluate;
+
+
---
+++ test/evaluator.js
@@ -0,0 +1,11 @@
+/*global describe: true, it: true */
+"use strict";
+
+var assert = require('assert');
+var parse = require('../build/reader').parser.parse;
+var evaluate = require('../src/evaluator').evaluate;
+
+var Symbol = require('../src/types').Symbol;
+
+describe('self-evaluating', function () {
+});
