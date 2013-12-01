5716d799026a8699adb5d71420876b7640e17fd8
Kris Jenkins <krisajenkins@gmail.com>
2013-12-01 11:52:37 +0000

    Symbols.

---
+++ src/types.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var Symbol = function (name) {
+    this.type = "Symbol";
+    this.name = name;
+    return this;
+};
+
+Symbol.prototype.equal = function (other) {
+    return ((this.type === other.type) && (this.name === other.name));
+};
+
+exports.Symbol = Symbol;
--- test/reader.js
+++ test/reader.js
@@ -3,6 +3,8 @@
 
 var assert = require('assert');
 var parse = require('../build/reader').parser.parse;
+var Symbol = require('../src/types').Symbol;
+
 
 describe('reader', function () {
 	it('Numbers', function () {
@@ -18,5 +20,11 @@ describe('reader', function () {
 		assert.deepEqual(parse('"test"'), "test");
 		assert.deepEqual(parse('"test(some)thing"'), 'test(some)thing');
 		assert.deepEqual(parse('"test[some]thing"'), 'test[some]thing');
+	});
+	it('Symbols', function () {
+		assert.deepEqual(parse("somevar"), new Symbol("somevar"));
+		assert.deepEqual(parse("+"), new Symbol("+"));
+		assert.deepEqual(parse("list?"), new Symbol("list?"));
+		assert.deepEqual(parse("<tom.dick_&-harry>"), new Symbol("<tom.dick_&-harry>"));
 	});
 });
